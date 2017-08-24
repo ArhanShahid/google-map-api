94acf9693815927ebb6039cd4b476c5de204d11d
Arhan Shahid <arhanshahid@gmail.com>
2017-08-24 15:39:53 +0500

    Bubble and Icon

--- scripts/currentGeoLocation.js
+++ scripts/currentGeoLocation.js
@@ -1,32 +1,25 @@
 $(function () {
 
-    var currentGeoLocationText = document.getElementById("currentGeoLocationText");
-
-
+    var currentGeoLocationText = $("#currentGeoLocationText");
 
     function getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition, showError);
         } else {
-            currentGeoLocationText.innerHTML = "Geolocation is not supported by this browser.";
+            currentGeoLocationText.text("Geolocation is not supported by this browser.");
         }
     }
 
     function showPosition(position) {
         lat = position.coords.latitude;
         lon = position.coords.longitude;
-        currentGeoLocationText.innerHTML = "Latitude: " + lat +
-            "<br>Longitude: " + lon;
+        currentGeoLocationText.text("Latitude: " + lat + " Longitude: " + lon);
         latlon = new google.maps.LatLng(lat, lon);
-        mapholder = document.getElementById('currentGeoLocationMapHolder');
-        mapholder.style.height = '90%';
-        mapholder.style.width = '100%';
 
         var myOptions = {
             center:latlon,
             zoom:14,
             mapTypeId:google.maps.MapTypeId.ROADMAP,
-            mapTypeControl:false,
             navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
         };
 
@@ -37,16 +30,16 @@ $(function () {
     function showError(error) {
         switch(error.code) {
             case error.PERMISSION_DENIED:
-                currentGeoLocationText.innerHTML = "User denied the request for Geolocation.";
+                currentGeoLocationText.text("User denied the request for Geolocation.");
                 break;
             case error.POSITION_UNAVAILABLE:
-                currentGeoLocationText.innerHTML = "Location information is unavailable.";
+                currentGeoLocationText.text("Location information is unavailable.");
                 break;
             case error.TIMEOUT:
-                currentGeoLocationText.innerHTML = "The request to get user location timed out.";
+                currentGeoLocationText.text("The request to get user location timed out.");
                 break;
             case error.UNKNOWN_ERROR:
-                currentGeoLocationText.innerHTML = "An unknown error occurred.";
+                currentGeoLocationText.text("An unknown error occurred.");
                 break;
         }
     }
--- scripts/markerIconAndMapBubble.js
+++ scripts/markerIconAndMapBubble.js
@@ -1,14 +1,41 @@
 $(function () {
 
     function initializeMarkerIconAndMapBubble() {
-        console.log('initializeMarkerIconAndMapBubble')
-        var addressMap;
-        var myOptions = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+        const markerIconAndMapBubbleLatLong = new google.maps.LatLng(24.8614622, 67.00993879999999);
+        const icon = {
+            size: new google.maps.Size(50, 100),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35),
+            url: 'images/map-location-pin.png'
         };
-        addressMap = new google.maps.Map(document.getElementById("markerIconAndMapBubbleMapHolder"), myOptions);
+        const bubbleString = '<div>'+
+            '<div >'+
+            '<h3>Bubble Heading</h3>'+
+            '</div>'+
+            '<div>'+
+            '<p> Bubble body content.</p>'+
+            '</div>'+
+            '</div>';
+
+        var infoWindow = new google.maps.InfoWindow({ content: bubbleString });
+        var myOptions = {
+            center: markerIconAndMapBubbleLatLong,
+            zoom: 14,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL}
+        };
+        var markerIconAndMapBubbleMap = new google.maps.Map(document.getElementById("markerIconAndMapBubbleMapHolder"), myOptions);
+        var marker = new google.maps.Marker({
+            position: markerIconAndMapBubbleLatLong,
+            map: markerIconAndMapBubbleMap,
+            icon: icon,
+            title: "Map Bubble Title!"
+        });
+        marker.addListener('click', function() {
+            infoWindow.open(markerIconAndMapBubbleMap, marker);
+        });
+
     }
 
     google.maps.event.addDomListener(window, 'load', initializeMarkerIconAndMapBubble);
