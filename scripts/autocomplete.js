190a93ccf7ca91e5b8da5fb0f52875af8941261a
Arhan Shahid <arhan.shahid@ext.lazada.com>
2019-01-30 15:19:13 +0500

    Code Refactor

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -1,21 +1,18 @@
 $(() => {
 
-    const options = {
+    const autocomplete = new google.maps.places.Autocomplete($('#google-place-ac')[0], {
         rankBy: google.maps.places.RankBy.DISTANCE,
         types: ['geocode', 'establishment'],
-        componentRestrictions: { country: 'PK' }
-    };
+        componentRestrictions: {
+            country: 'PK'
+        }
+    });
 
-    const autocomplete = new google.maps.places.Autocomplete($('#google_places_ac')[0], options);
-    
     google.maps.event.addListener(autocomplete, 'place_changed', () => {
         const location = autocomplete.getPlace();
         if (location && location.geometry) {
-            $('#googlePlaceAutoCompleteText').html(`Latitude: ${location.geometry.location.lat()} Longitude: ${location.geometry.location.lng()}`);
-            console.log({
-                'Latitude:': location.geometry.location.lat(),
-                'Longitude:': location.geometry.location.lng()
-            });
+            $('#google-place-ac-text').html(`Latitude: ${location.geometry.location.lat()} Longitude: ${location.geometry.location.lng()}`);
+            console.log(`Latitude: ${location.geometry.location.lat()} Longitude: ${location.geometry.location.lng()}`);
         }
     });
 });
